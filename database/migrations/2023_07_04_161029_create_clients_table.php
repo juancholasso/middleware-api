b64609c9e0d1161156c6c1af7d97c99ee902ec5a
Juan Pablo <juancholasso@hotmail.com>
2023-07-07 12:10:11 -0500

    Added Changes

---
+++ database/migrations/2023_07_04_161029_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('hook_key');
+            $table->string('zkong_account');
+            $table->string('zkong_password');
+            $table->string('zkong_attr_category');
+            $table->string('zkong_attr_name');
+            $table->string('zkong_store_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
--- database/migrations/2023_07_05_173728_create_hook_requests_table.php
+++ database/migrations/2023_07_05_173728_create_hook_requests_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('hook_requests', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("client_id");
             $table->unsignedBigInteger("id_hook_identifier")->nullable();
             $table->longText("payload")->nullable();
             $table->timestamp("hook_received_date");
@@ -24,6 +25,11 @@ return new class extends Migration
             $table->integer("api_attempt")->default(1);
             $table->timestamp("api_request_date", $precision = 0)->nullable();
             $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('clients')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
         });
     }
 
